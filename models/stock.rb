052ff083fca4add75c0a1f2a555cf4c804f31700
James Scott <jamessct@hotmail.co.uk>
2016-08-14 15:32:48 +0100

    def init for stock

---
+++ models/stock.rb
@@ -0,0 +1,18 @@
+require('pg')
+require_relative('..db/sql_runner')
+require_relative('..models/album')
+
+class Stock
+
+  attr_reader(:id, :album_id, :format, :stock_level, :threshold, :buy_price, :sell_price)
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @album_id = options['album_id'].to_i
+    @format = options['format']
+    @stock_level = options['stock_level'].to_i
+    @threshold = options['threshold'].to_i
+    @buy_price = options['buy_price'].to_i
+    @sell_price = options['sell_price'].to_i
+  end
+end
