0a489d09f4051cf8bbd1d95de8dbe0aff86d2213
James Scott <jamessct@hotmail.co.uk>
2016-08-14 16:06:20 +0100

    added to spec files

---
+++ specs/album_spec.rb
@@ -0,0 +1,9 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('..models/album')
+
+def setup
+end
+
+def test_name
+end
---
+++ specs/stock_spec.rb
@@ -0,0 +1,6 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../models/stock')
+
+def setup
+end
