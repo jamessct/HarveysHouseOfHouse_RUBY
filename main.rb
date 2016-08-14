d200f5ebff37cebd183fe4951ec264669df625e4
James Scott <jamessct@hotmail.co.uk>
2016-08-14 13:15:53 +0100

    some updates to controllers

---
+++ main.rb
@@ -0,0 +1,7 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('controllers/artists_controller.rb')
+require_relative('controllers/albums_controller.rb')
+require_relative('controllers/stock_controller.rb')
+
